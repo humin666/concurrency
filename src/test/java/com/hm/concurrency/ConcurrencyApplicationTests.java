180cd62dfc1b17807a6f0de9312143f053973405
humin666 <781876878@qq.com>
2020-03-10 15:30:39 +0800

    初始化工程

---
+++ src/test/java/com/hm/concurrency/ConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hm.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
